88b48a8d2c1065c7a0ccaf0284780eb4465fa133
Leo Lu <luyuxuanleo@gmail.com>
2022-01-21 17:40:41 +0800

    Parallel Linear

--- __main__.py
+++ __main__.py
@@ -1,33 +1,46 @@
 import argparse
-from .matrix import ParalleledMatrixMulply
+from matrix import ParalleledMatrixMulply
 from IPython import embed
 import torch
 import torch.distributed as dist
 import pytorch_lightning as pl
+from tqdm import tqdm
 
 pl.seed_everything(1234)
 
 parser = argparse.ArgumentParser(description='PyTorch distributed training on cifar-10')
 parser.add_argument('--rank', default=0, type=int,
                     help='rank of current process')
-parser.add_argument('--word_size', default=2, type=int,
+parser.add_argument('--world_size', default=2, type=int,
                     help="word size")
 parser.add_argument('--init_method', default='tcp://127.0.0.1:23456',
                     help="init-method")
 args = parser.parse_args()
 
-dist.init_process_group(backend='nccl', init_method=args.init_method, rank=args.rank, world_size=args.word_size)
+dist.init_process_group(backend='nccl', init_method=args.init_method, rank=args.rank, world_size=args.world_size)
 
-net = ParalleledMatrixMulply(4,4)
-# net = net.cuda()
 device = torch.device('cuda', args.rank)
+size = 15000
+net = ParalleledMatrixMulply(size, size, device)
+# net = net.cuda()
 net = net.to(device)
-net.train()
-# net = torch.nn.parallel.DistributedDataParallel(net)
 
-A = torch.rand(4, 4).to(device)
-B = torch.rand(4, 4).to(device)
-C = net(A, B)
-print(C - torch.matmul(A, B))
+
+optim = torch.optim.Adam(net.parameters(), 1)
+for i in tqdm(range(10)):
+    optim.zero_grad()
+    A = torch.rand(size, size).to(device)
+    C = net(A)
+    C = torch.sum(C, 1)
+    # C = torch.softmax(C, 0)
+    loss = C[0]
+    loss.backward()
+    optim.step()
+    print(f"{loss=}")
+# print(C - torch.matmul(A, B))
 
 embed()
+
+
+# world_size=2: 9203MiB
+# world_size=1: 13559MiB
--- matrix.py
+++ matrix.py
@@ -2,6 +2,7 @@ import torch.distributed as dist
 import torch.utils.data.distributed
 import torch
 import torch.nn.functional as F
+from IPython import embed
 
 # class F(torch.autograd.Function):
     
@@ -13,33 +14,43 @@ import torch.nn.functional as F
 #     def backward(ctx, grad_output):
 #         dist.all_reduce(grad_output)
 
+class f(torch.autograd.Function): 
+    @staticmethod
+    def forward(ctx, x): 
+        return x 
+    @staticmethod
+    def backward(ctx, gradient): 
+        dist.all_reduce(gradient) 
+        return gradient
+
+class g(torch.autograd.Function): 
+    @staticmethod
+    def forward(ctx, x): 
+        dist.all_reduce(x) 
+        return x 
+    @staticmethod
+    def backward(ctx, gradient): 
+        return gradient
 
 class ParalleledMatrixMulply(torch.nn.Module):
-
-    def __init__(self, size1, size2):
+    def __init__(self, size1, size2, device):
         super().__init__()
+        self.size1 = size1
+        self.size2 = size2
         self.world_size = dist.get_world_size()
         self.rank = dist.get_rank()
         self.bias = torch.nn.Parameter(torch.zeros(size1, size2))
-        
-    def forward(self, A, B):
+        self.weight = torch.nn.Parameter(torch.rand(size1 // self.world_size, size2))
+        self.f = f.apply
+        self.g = g.apply
+
+    def forward(self, X):
         # col-wize parallel
-        print(A.size(1) // self.world_size * self.rank, A.size(1) // self.world_size * (self.rank + 1))
-        A = A[:, A.size(1) // self.world_size * self.rank : A.size(1) // self.world_size * (self.rank + 1)]
-        # row-wize parallel
-        B = B[B.size(0) // self.world_size * self.rank : B.size(0) // self.world_size * (self.rank + 1), :]
-        Y = torch.matmul(A, B)
-        dist.all_reduce(Y)
+        # print(A.size(1) // self.world_size * self.rank, A.size(1) // self.world_size * (self.rank + 1))
+        weight = self.f(self.weight)
+        # B = self.f(B)
+        X = X[:, X.size(1) // self.world_size * self.rank : X.size(1) // self.world_size * (self.rank + 1)]
+        Y = torch.matmul(X, weight)
+        Y = self.g(Y).clone()
         Y += self.bias
         return Y
-
-    # def backward(ctx, grad_output):
-    #     """
-    #     In the backward pass we receive a Tensor containing the gradient of the loss
-    #     with respect to the output, and we need to compute the gradient of the loss
-    #     with respect to the input.
-    #     """
-    #     input, = ctx.saved_tensors
-    #     grad_input = grad_output.clone()
-    #     grad_input[input < 0] = 0
-    #     return grad_input
